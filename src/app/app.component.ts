2bb64b7c52a28c19b51d22461a7ff1d2a8bcfc16
fichimura <fabichimura@gmail.com>
2024-06-03 00:04:03 -0300

    add auth listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './pages/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'e-morty';
+export class AppComponent implements OnInit {
+  title = 'e-morty';  
+
+  constructor(private authService: AuthService){}
+
+  ngOnInit() {
+      this.authService.initAuthListener();
+  }
 }
