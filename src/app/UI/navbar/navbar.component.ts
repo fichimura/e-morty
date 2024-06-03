f7bfcc13eda13d974f6ec556273d6cd56038426c
fichimura <fabichimura@gmail.com>
2024-06-03 00:00:26 -0300

    navbar according to if user id authenticated or not

--- src/app/UI/navbar/navbar.component.ts
+++ src/app/UI/navbar/navbar.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { AuthService } from '../../pages/auth/auth.service';
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
+  isAuth = false;
+  authSubscription: Subscription;
 
+  constructor(private authService: AuthService){}
+
+  ngOnInit(){
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
+  }
+
+  onLogout(){
+    this.authService.logout();
+  }
+
+  ngOnDestroy(){
+    this.authSubscription.unsubscribe();
+  }
 }
