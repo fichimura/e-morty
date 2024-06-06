d73bc6ffaca30cfc5522d9566364b3da11f0c340
fichimura <fabichimura@gmail.com>
2024-06-06 01:07:59 -0300

    getUser from firebase

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,24 @@
-import { Subject } from "rxjs";
+import { Observable, Subject } from "rxjs";
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 import { AuthData } from "../models/auth-data.model";
+import { User } from "../models/user.model";
 
 @Injectable()
 export class AuthService{
     authChange = new Subject<boolean>();
     private isAuthenticated = false;
+    private userId: string;
 
-    constructor(private router: Router,private angularFireAuth: AngularFireAuth ){}
+    constructor(private router: Router,private angularFireAuth: AngularFireAuth, private firestore: AngularFirestore ){}
 
     initAuthListener(){
         this.angularFireAuth.authState.subscribe(user =>{
             if(user){
+                this.userId = user.uid;
                 this.isAuthenticated = true;
                 this.authChange.next(true);
                 this.router.navigate(['/']);
@@ -26,12 +30,20 @@ export class AuthService{
         })
     }
 
-    registerUser(authData: AuthData){
+    registerUser(authData: AuthData, userData: User ){
         this.angularFireAuth.createUserWithEmailAndPassword(
             authData.email,
             authData.password
         ).then(result =>{
             console.log(result);
+            if(result.user){
+                this.firestore.collection('users').doc(result.user.uid).set(userData)
+                    .then(() => {
+                        console.log("User data saved successfully");
+                    }).catch(error => {
+                        console.log("There was an error", error);
+                    })
+            }
         }).catch(error => {
             console.log(error);
         });
@@ -54,5 +66,9 @@ export class AuthService{
 
     isAuth(){
         return this.isAuthenticated;
+    }
+
+    getUserData(): Observable<any>{
+        return this.firestore.collection('users').doc(this.userId).valueChanges();
     }
 }
