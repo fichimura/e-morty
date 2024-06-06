b32446469a65d99c434739cdd081a965da0da5ef
fichimura <fabichimura@gmail.com>
2024-06-06 01:08:10 -0300

    show user logged in

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+  user: any;
+
+  constructor(private authService: AuthService){}
+
+  ngOnInit(): void {
+    this.authService.getUserData().subscribe(userData => {
+      this.user = userData;
+    });
+  }
 
 }
