68d9ea2a10322d61094156eaaa8fc3d29e29bbc9
fichimura <fabichimura@gmail.com>
2024-06-06 16:37:39 -0300

    add generic error component

---
+++ src/app/UI/error-handling/error-handling.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-error-handling',
+  templateUrl: './error-handling.component.html',
+  styleUrl: './error-handling.component.scss'
+})
+export class ErrorHandlingComponent {
+  @Input() message: string;
+
+}
