f790fd36fe4f8a08f51c4a54bcb9fc8e263eed5f
fichimura <fabichimura@gmail.com>
2024-06-06 16:37:56 -0300

    sign in and sign up errors handled

--- src/app/pages/auth/signin/signin.component.ts
+++ src/app/pages/auth/signin/signin.component.ts
@@ -8,14 +8,40 @@ import { AuthService } from '../../../services/auth.service';
   styleUrl: '../auth.scss'
 })
 export class SigninComponent {
+  signedInSuccessfully: boolean = true;
+  errorMessage: string;
 
   constructor(private authService: AuthService){
   }
 
-  onSubmit(form: NgForm){
-    this.authService.login({
+  onSubmit(form: NgForm): void{
+    this.authService.login(
+      {
       email: form.value.email,
       password: form.value.password
+    }).then(result => {
+      this.signedInSuccessfully = true;
+    }).catch(error => {
+      this.signedInSuccessfully = false;
+      this.errorMessage = this.handleSignInError(error.code);
     });
+  }
+
+  handleSignInError(code: string): string{
+    switch(code){
+      case 'auth/internal-error' : {
+        return 'An internal error has occurred. Try to sign again, and if the error persists contact us.'
+      }case 'auth/invalid-email': {
+        return 'You provided an invalid email. Insert a valid one and try sign in again.';
+      }case 'auth/missing-password': {
+        return 'You must provide a password to sign in. Fill the password field and try sign in again.';
+      }case 'auth/invalid-password': {
+        return 'The password that you provided is invalid. The password must be at least six characters. Review it and try sign in again.'
+      }case 'auth/invalid-credential' : {
+        return 'Email or password incorrect. Review both and try sign in again.'
+      }default:{
+        return 'There was an error when sign in. Try again.';
+      }
+    }
   }
 }
--- src/app/pages/auth/signup/signup.component.ts
+++ src/app/pages/auth/signup/signup.component.ts
@@ -8,6 +8,8 @@ import { AuthService } from '../../../services/auth.service';
   styleUrl: '../auth.scss'
 })
 export class SignupComponent {
+  signedUpSuccessfully: boolean = true;
+  errorMessage: string;
 
   constructor(private authService: AuthService){}
 
@@ -22,6 +24,30 @@ export class SignupComponent {
         country: form.value.country,
         email: form.value.email
       }
-    );
-  } 
+    ).then(result =>{
+      this.signedUpSuccessfully = true;
+    }).catch(error => {
+      this.signedUpSuccessfully = false;
+      this.errorMessage = this.handleSignUpError(error.code);
+    });
+  }
+
+  handleSignUpError(code: string): string{
+    switch(code){
+      case 'auth/internal-error' : {
+        return 'An internal error has occurred. Try to sign again, and if the error persists contact us.'
+      }case 'auth/invalid-email': {
+        return 'You provided an invalid email. Insert a valid one and try sign in again.';
+      }case 'auth/missing-password': {
+        return 'You must provide a password to sign in. Fill the password field and try sign in again.';
+      }case 'auth/invalid-password': {
+        return 'The password that you provided is invalid. The password must be at least six characters. Review it and try sign in again.'
+      }case 'auth/invalid-credential' : {
+        return 'Email or password incorrect. Review both and try sign in again.'
+      }default:{
+        return 'There was an error when sign in. Try again.';
+      }
+    }
+  }
+
 }
