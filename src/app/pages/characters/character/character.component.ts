d5954cebe9976f824a62d603b195f89209da439f
fichimura <fabichimura@gmail.com>
2024-06-07 01:03:49 -0300

    enable go to episodes of the character, to its location and its origin

--- src/app/pages/characters/character/character.component.ts
+++ src/app/pages/characters/character/character.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FetchApiService } from '../../../services/fetchApi.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Character } from '../../../models/character.model';
 import { Subscription } from 'rxjs';
 
@@ -9,16 +9,21 @@ import { Subscription } from 'rxjs';
   templateUrl: './character.component.html',
   styleUrl: './character.component.scss'
 })
-export class CharacterComponent implements OnInit, OnDestroy { 
-  getSubjectSubscription: Subscription;
+export class CharacterComponent implements OnInit, OnDestroy {
+  private getMultipleSubjects: Subscription;
+  private getSubjectSubscription: Subscription;
   
   loading = false;
+  loadingEpisodes = false;
 
   character: Character | undefined;
   characterId: string;
   characterEpisodes: string[] = [];
+  characterEpisodesObjects: any = [];
 
-  constructor(private fetchApiService: FetchApiService, private route: ActivatedRoute){}
+  constructor(private fetchApiService: FetchApiService,
+              private route: ActivatedRoute,
+              private router: Router){}
 
   ngOnInit(): void{
     this.characterId = this.route.snapshot.paramMap.get('characterId');
@@ -29,7 +34,7 @@ export class CharacterComponent implements OnInit, OnDestroy {
 
   getCharacter(): void{
     this.loading = true;
-    this.getSubjectSubscription = this.fetchApiService.getSubject(this.characterId, 'character').subscribe(
+    this.getSubjectSubscription = this.fetchApiService.getSubject('character', this.characterId).subscribe(
       {
       next: response => {
         this.character = response;
@@ -44,15 +49,44 @@ export class CharacterComponent implements OnInit, OnDestroy {
             const partsOfUrl = episode.split('/');
             const episodeNumber = partsOfUrl[partsOfUrl.length - 1];
             this.characterEpisodes.push(episodeNumber); 
-          })
-
+          });
           this.loading = false;
+          this.loadCharacterEpisodes(this.characterEpisodes);
         }
       }
     )
   }
 
+  loadCharacterEpisodes(episodesIds: string[]): void{
+    this.loadingEpisodes = true;
+    this.getMultipleSubjects = this.fetchApiService.getMultipleSubjects('episode', episodesIds).subscribe({
+      next: response => {
+        this.characterEpisodesObjects = response;
+        this.loadingEpisodes = false;
+      },
+      error: error => {
+        console.log(error);
+        this.loadingEpisodes = false;
+      }
+    });
+  }
+
+  onLocationClicked(locationUrl: string): void{
+    const locationId = locationUrl.split('/').pop();
+    this.router.navigate(['/location', locationId]);
+  }
+
+  onOriginClicked(locationUrl: string): void{
+    const locationId = locationUrl.split('/').pop();
+    this.router.navigate(['/location', locationId]);
+  }
+
+  onEpisodeSelected(episodeId: string): void{
+    this.router.navigate(['/episode', episodeId]);
+  }
+
   ngOnDestroy(): void {
     this.getSubjectSubscription.unsubscribe();
+    this.getMultipleSubjects.unsubscribe();
   }
 }
