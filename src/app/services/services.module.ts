7a368f2d3e4cb02e2e65e58a3046219ae9398196
fichimura <fabichimura@gmail.com>
2024-06-07 15:20:55 -0300

    services module created

---
+++ src/app/services/services.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { FetchApiService } from "./fetchApi.service";
+
+@NgModule({
+    providers: [
+        FetchApiService
+    ]
+})
+
+export class ServicesModule { }
